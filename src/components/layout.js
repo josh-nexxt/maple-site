a6c291a9b23de761ee0366e0ed7dfaba7ba2ddb9
Josh <66446168+josh-nexxt@users.noreply.github.com>
2021-06-04 19:34:24 -0400

    Update meta description

--- src/components/layout.js
+++ src/components/layout.js
@@ -27,17 +27,17 @@ const Layout = ({ children, location }) => {
             title={data.site.siteMetadata.title}
             meta={[
               { name: 'title', content: 'MAPLE beta 2.1' },
-              { name: 'description', content: 'A cultural assessment tool from the Intercultural Skills Lab' },
+              { name: 'description', content: 'MAPLE is a cultural assessment tool designed at the Rotman School that helps people understand their place in a diverse and multicultural workplace. Try the beta version of this tool and get a first glimpse of how MAPLE is set to change the way we relate to our work, and each other.' },
               { name: 'keywords', content: 'maple, intercultural' },
               { name: 'og:type', content: 'website' },
               { name: 'og:url', content: 'https://interculturalskillslab.com' },
               { name: 'og:title', content: 'MAPLE beta 2.1' },
-              { name: 'og:description', content: 'A cultural assessment tool from the Intercultural Skills Lab' },
+              { name: 'og:description', content: 'MAPLE is a cultural assessment tool designed at the Rotman School that helps people understand their place in a diverse and multicultural workplace. Try the beta version of this tool and get a first glimpse of how MAPLE is set to change the way we relate to our work, and each other.' },
               { name: 'og:image', content: `${data.site.siteMetadata.url}/preview.png` },
               { name: 'twitter:card', content: 'summary_large_image' },
               { name: 'twitter:url', content: 'https://interculturalskillslab.com' },
               { name: 'twitter:title', content: 'MAPLE beta 2.1' },
-              { name: 'twitter:description', content: 'A cultural assessment tool from the Intercultural Skills Lab' },
+              { name: 'twitter:description', content: 'MAPLE is a cultural assessment tool designed at the Rotman School that helps people understand their place in a diverse and multicultural workplace. Try the beta version of this tool and get a first glimpse of how MAPLE is set to change the way we relate to our work, and each other.' },
               { name: 'twitter:image', content: `${data.site.siteMetadata.url}/preview.png` },
 
             ]}
