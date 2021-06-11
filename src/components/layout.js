dd683c49a26016f6f555ca61d4a45ce242caa4ad
Josh Seltzer <jyseltz@gmail.com>
2021-06-11 14:19:41 -0400

    fix meta key

--- src/components/layout.js
+++ src/components/layout.js
@@ -39,7 +39,7 @@ const Layout = ({ children, location }) => {
             <meta key={10} name='twitter:title' content='MAPLE beta 2.1' />
             <meta key={11} name='twitter:description' content='MAPLE is a cultural assessment tool designed at the Rotman School that helps people understand their place in a diverse and multicultural workplace. Try the beta version of this tool and get a first glimpse of how MAPLE is set to change the way we relate to our work, and each other.' />
             <meta key={12} name='twitter:url' content='https://interculturalskillslab.com' />
-            <meta key={7} name='twitter:image' content={`${data.site.siteMetadata.url}/preview.png`} />
+            <meta key={13} name='twitter:image' content={`${data.site.siteMetadata.url}/preview.png`} />
           </Helmet>
           <div>
             {children}
