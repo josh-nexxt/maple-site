c06c6890ef814a862cf175e5a55d51affb60b25c
Josh Seltzer <jyseltz@gmail.com>
2021-06-13 23:50:12 -0400

    only disable `og:description`

--- src/components/layout.js
+++ src/components/layout.js
@@ -27,7 +27,7 @@ const Layout = ({ children, location }) => {
             title={data.site.siteMetadata.title}
           >
             <html lang="en" />
-            {/* <meta key={1} name='description' content='MAPLE is a cultural assessment tool designed at the Rotman School that helps people understand their place in a diverse and multicultural workplace. Try the beta version of this tool and get a first glimpse of how MAPLE is set to change the way we relate to our work, and each other.' /> */}
+            <meta key={1} name='description' content='MAPLE is a cultural assessment tool designed at the Rotman School that helps people understand their place in a diverse and multicultural workplace. Try the beta version of this tool and get a first glimpse of how MAPLE is set to change the way we relate to our work, and each other.' />
             <meta key={2} name='keywords' content='maple, intercultural' />
             <meta key={3} name='og:type' content='website' />
             <meta key={4} name='og:url' content='https://interculturalskillslab.com' />
@@ -37,7 +37,7 @@ const Layout = ({ children, location }) => {
             <meta key={8} name='twitter:card' content='summary_large_image' />
             <meta key={9} name='twitter:url' content='https://interculturalskillslab.com' />
             <meta key={10} name='twitter:title' content='MAPLE beta 2.1' />
-            {/* <meta key={11} name='twitter:description' content='MAPLE is a cultural assessment tool designed at the Rotman School that helps people understand their place in a diverse and multicultural workplace. Try the beta version of this tool and get a first glimpse of how MAPLE is set to change the way we relate to our work, and each other.' /> */}
+            <meta key={11} name='twitter:description' content='MAPLE is a cultural assessment tool designed at the Rotman School that helps people understand their place in a diverse and multicultural workplace. Try the beta version of this tool and get a first glimpse of how MAPLE is set to change the way we relate to our work, and each other.' />
             <meta key={12} name='twitter:url' content='https://interculturalskillslab.com' />
             <meta key={13} name='twitter:image' content={`${data.site.siteMetadata.url}/preview.png`} />
           </Helmet>
